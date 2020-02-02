346349b4ba91f7c841502155be83420e9dcc5eb6
Lauren Oey <loey@ucsd.edu>
2020-02-02 02:22:36 -0800

    get neighbors topic distribution with same neighbors from previous year

--- R/cogsci_analysis/influenceNeighbors/cogsci_influence_neighbors.R
+++ R/cogsci_analysis/influenceNeighbors/cogsci_influence_neighbors.R
@@ -2,7 +2,7 @@ setwd("/Users/loey/Desktop/Research/InfluencingCogSci/R/cogsci_analysis/influenc
 library(tidyverse)
 library(futile.matrix)
 library(pbapply)
-topic.df <- read_csv("cogsci_topics_authorAbbr.csv") %>%
+topic.df <- read_csv("fullcogsci_topics_authorAbbr.csv") %>%
   distinct() %>%
   mutate(authors=ifelse(grepl(",",authors), gsub(",.*","",authors), authors),
          authors=ifelse(authors=="J Tenenbaums", "J Tenenbaum", authors)) %>%
@@ -10,8 +10,8 @@ topic.df <- read_csv("cogsci_topics_authorAbbr.csv") %>%
 
 full_author_neighbor.df = c()
 neighbor_mats_final = list()
-years = 2000:2019
-for(i in 1:20){
+years = 1981:2019
+for(i in 1:length(years)){
   temp = readRDS(paste0("neighbors/neighbors_",years[i],".rds"))
   diag(temp) <- NA # removing self co-authorship binary info too
   neighbor_mats_final[[i]] <- temp 
@@ -35,13 +35,23 @@ get_avg_topic_dist <- function(df) {
 }
 
 get_neighbor_topic_dist_byYear <- function(year.select, author, neighbor.dist=2:4){
-  neighbor.matr = neighbor_mats_final[[year.select-1999]][author,]
+  neighbor.matr = neighbor_mats_final[[year.select-1980]][author,]
   neighbors = neighbor.matr[which(neighbor.matr %in% neighbor.dist)]
   neighbor.topics <- topic.df %>%
     filter(year==year.select & length(neighbors > 0) & authors %in% names(neighbors)) %>%
     get_avg_topic_dist() %>%
     as.list()
   return(neighbor.topics)
+}
+
+get_neighbor_topic_dist_byYear.next <- function(year.select, author, neighbor.dist=2:4){
+  neighbor.matr = neighbor_mats_final[[year.select-1980]][author,]
+  neighbors = neighbor.matr[which(neighbor.matr %in% neighbor.dist)]
+  neighbor.topics.next <- topic.df %>%
+    filter(year==(year.select+1) & length(neighbors > 0) & authors %in% names(neighbors)) %>%
+    get_avg_topic_dist() %>%
+    as.list()
+  return(neighbor.topics.next)
 }
 
 get_projection_angle = function(vec.a, vec.b) {
@@ -69,23 +79,24 @@ allAuthorTopicDist <- allTopicDist %>%
   mutate(topic = paste0("topic",str_pad(topic, 3, pad = "0")))
 #write_csv(allAuthorTopicDist, "avg_topic_dist_author_year.csv")
 
-# Author's topic distribution for previous year
-allAuthorTopicDist.prevYr <- allAuthorTopicDist %>%
-  filter(year < 2019) %>%
-  mutate(prev.year = year,
-         year = prev.year+1,
-         prev.author_mean = author_mean) %>%
+# Author's topic distribution for next year
+allAuthorTopicDist.nextYr <- allAuthorTopicDist %>%
+  filter(year > 1981) %>%
+  mutate(year = year-1,
+         next.year = year+1,
+         next.author_mean = author_mean) %>%
   dplyr::select(-author_mean)
 
-allAuthorTopicDist.time <- full_join(allAuthorTopicDist, allAuthorTopicDist.prevYr)
+allAuthorTopicDist.time <- full_join(allAuthorTopicDist, allAuthorTopicDist.nextYr)
 #write_csv(allAuthorTopicDist.time, "avg_topic_dist_author_this_prev.csv")
 
 allAuthorTopicDist.diff <- allAuthorTopicDist.time %>%
   na.omit() %>%
-  mutate(diff.author_mean = author_mean-prev.author_mean)
+  mutate(diff.author_mean = next.author_mean-author_mean)
+nrow(allAuthorTopicDist.diff)/100 #7,740 authors with papers contributed in consecutive years
 
 # Each author's neighbors' topic distribution for a given year
-allNeighborTopicDist <-  allTopicDist %>%
+allNeighborTopicDist <- allTopicDist %>%
   dplyr::select(year, authors) %>%
   mutate(neighbor_mean = pbmapply(get_neighbor_topic_dist_byYear,year,authors)) %>%
   unnest() %>%
@@ -93,49 +104,53 @@ allNeighborTopicDist <-  allTopicDist %>%
   mutate(topic = paste0("topic",str_pad(rep(1:100, nrow(allTopicDist)), 3, pad = "0")))
 #write_csv(allNeighborTopicDist, "avg_topic_dist_neighbors_year.csv")
 
-# Author's neighbors' topic distribution for previous year
-allNeighborTopicDist.prevYr <- allNeighborTopicDist %>%
+# Author's neighbors' topic distribution for next year
+allNeighborTopicDist.nextYr <- allTopicDist %>%
   filter(year < 2019) %>%
-  mutate(prev.year = year,
-         year = prev.year+1,
-         prev.neighbor_mean = neighbor_mean) %>%
+  dplyr::select(year, authors) %>%
+  mutate(neighbor_mean = pbmapply(get_neighbor_topic_dist_byYear.next,year,authors)) %>%
+  unnest() %>%
+  ungroup() %>%
+  mutate(topic = paste0("topic",str_pad(rep(1:100, nrow(filter(allTopicDist, year<2019))), 3, pad = "0")),
+         next.year = year+1,
+         next.neighbor_mean = neighbor_mean) %>%
   dplyr::select(-neighbor_mean)
 
-allNeighborTopicDist.time <- full_join(allNeighborTopicDist, allNeighborTopicDist.prevYr)
+allNeighborTopicDist.time <- full_join(allNeighborTopicDist, allNeighborTopicDist.nextYr)
 #write_csv(allNeighborTopicDist.time, "avg_topic_dist_neighbors_this_prev.csv")
 
 allNeighborTopicDist.diff <- allNeighborTopicDist.time %>%
   na.omit() %>%
-  mutate(diff.neighbor_mean = neighbor_mean-prev.neighbor_mean)
+  mutate(diff.neighbor_mean = next.neighbor_mean-neighbor_mean)
+nrow(allNeighborTopicDist.diff)/100 #8,962 authors's neighbors with papers contributed in consecutive years
 
-combinedTopicDist <- full_join(allAuthorTopicDist.diff, allNeighborTopicDist.diff, by=c("year","authors","topic","prev.year")) %>%
+combinedTopicDist <- full_join(allAuthorTopicDist.diff, allNeighborTopicDist.diff, by=c("year","authors","topic","next.year")) %>%
   na.omit() %>%
-  mutate(diff.prev.neighbor_author_mean = prev.neighbor_mean - prev.author_mean)
-nrow(combinedTopicDist) #2,687 distinct authors/years
+  mutate(diff.neighbor_author_mean = neighbor_mean - author_mean)
+nrow(combinedTopicDist)/100 #2,687 (previously), 3,347 (now) distinct authors/years 
 
 all_proj_angles <- combinedTopicDist %>%
-  group_by(authors, prev.year) %>%
-  summarise(proj_scalar_author = get_projection_angle(diff.author_mean, diff.prev.neighbor_author_mean)*sqrt(sum(diff.author_mean^2)),
-            proj_scalar_neighbors = get_projection_angle(diff.neighbor_mean, diff.prev.neighbor_author_mean)*sqrt(sum(diff.neighbor_mean^2)))
+  group_by(authors, year) %>%
+  summarise(proj_scalar_author = get_projection_angle(diff.author_mean, diff.neighbor_author_mean)*sqrt(sum(diff.author_mean^2)),
+            proj_scalar_neighbors = get_projection_angle(diff.neighbor_mean, diff.neighbor_author_mean)*sqrt(sum(diff.neighbor_mean^2)))
 all_proj_angles
 write_csv(all_proj_angles, "all_projection_angles.csv")
 
 ggplot(all_proj_angles, aes(x=proj_scalar_author, y=proj_scalar_neighbors)) +
   geom_point() +
   geom_rug()
-
+ggsave("authors_neighbors.png")
 
 
 
 # Correlate with centrality
 all_centrality <- data.frame()
-for(i in 2000:2019){
+for(i in 1981:2019){
   year_centrality <- read_csv(paste0("../networkByYear/centrality_",i,".csv")) %>%
     mutate(year=i)
   all_centrality <- bind_rows(all_centrality, year_centrality) %>%
     dplyr::select(-X1)
 }
-unique(all_centrality$CM)
 
 all_centrality <- all_centrality %>%
   spread(CM, measure) %>%
@@ -146,8 +161,6 @@ all_centrality <- all_centrality %>%
 
 
 all_proj_angles_central <- all_proj_angles %>%
-  mutate(year = prev.year) %>%
-  dplyr::select(-prev.year) %>%
   left_join(all_centrality) %>%
   mutate(logit.eigen = ifelse(eigen==0, -99,
                               ifelse(eigen==1, 99,
@@ -194,7 +207,8 @@ ggplot(all_proj_angles_central, aes(x=log(degree), y=proj_scalar_author)) +
   geom_smooth(method=lm)
 
 ggplot(all_proj_angles_central, aes(x=log(close), y=proj_scalar_neighbors)) +
-  geom_point()
+  geom_point() +
+  geom_smooth(method=lm)
 
 
 #### NULL SHUFFLE PROJECTION AND CENTRALITY ####
@@ -205,22 +219,26 @@ corr<- function(dat){
 
 dat.shuffle.degree.neighbors <- function(dat){
   data.frame(central=log(dat$degree), 
-             proj_scalar=sample(dat$proj_scalar_neighbors, length(dat$proj_scalar_neighbors), replace=F))
+             proj_scalar=sample(dat$proj_scalar_neighbors, length(dat$proj_scalar_neighbors), replace=F)) %>%
+    na.omit()
 }
 
 dat.shuffle.degree.author <- function(dat){
   data.frame(central=log(dat$degree), 
-             proj_scalar=sample(dat$proj_scalar_author, length(dat$proj_scalar_author), replace=F))
+             proj_scalar=sample(dat$proj_scalar_author, length(dat$proj_scalar_author), replace=F)) %>%
+    na.omit()
 }
 
 dat.shuffle.eigen.neighbors <- function(dat){
   data.frame(central=dat$logit.eigen, 
-             proj_scalar=sample(dat$proj_scalar_neighbors, length(dat$proj_scalar_neighbors), replace=F))
+             proj_scalar=sample(dat$proj_scalar_neighbors, length(dat$proj_scalar_neighbors), replace=F)) %>%
+    na.omit()
 }
 
 dat.shuffle.eigen.author <- function(dat){
   data.frame(central=dat$logit.eigen, 
-             proj_scalar=sample(dat$proj_scalar_author, length(dat$proj_scalar_author), replace=F))
+             proj_scalar=sample(dat$proj_scalar_author, length(dat$proj_scalar_author), replace=F)) %>%
+    na.omit()
 }
 
 
